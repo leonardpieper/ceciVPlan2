555326767970bfe96726327ef4e1c027b36311e4
leonardpieper <leonard.pieper@outlook.com>
2017-03-18 20:39:42 +0100

    Easter egg improved

--- app/src/main/java/com/github/leonardpieper/ceciVPlan/tools/EasterEgg.java
+++ app/src/main/java/com/github/leonardpieper/ceciVPlan/tools/EasterEgg.java
@@ -4,6 +4,14 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.NotificationCompat;
 
@@ -25,7 +33,8 @@ public class EasterEgg {
         android.support.v4.app.NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(mContext)
                         .setSmallIcon(R.mipmap.ic_launcher_alpha)
-                        .setContentTitle("\uD83D\uDC36");
+                        .setContentTitle("\uD83D\uDC36" + " Du hast den Hund gefunden")
+                        .setContentText("1/1 gefundene Emojis");
         // Creates an explicit intent for an Activity in your app
         Intent resultIntent = new Intent(mContext, MainActivity.class);
 
@@ -48,5 +57,44 @@ public class EasterEgg {
                 (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
         // mId allows you to update the notification later on.
         mNotificationManager.notify(9000, mBuilder.build());
+    }
+
+    public Bitmap makIcon(){
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setTextSize(12);
+        paint.setColor(12);
+        paint.setTextAlign(Paint.Align.LEFT);
+        float baseline = -paint.ascent();
+
+        int width = (int) (paint.measureText("\uD83D\uDC36") + 0.5f);
+        int height = (int) (baseline + paint.descent() + 0.5f);
+
+        Bitmap image = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(image);
+        canvas.drawText("\uD83D\uDC36", 0, baseline, paint);
+        return image;
+    }
+
+    private Bitmap getCircleBitmap(Bitmap bitmap) {
+        final Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+                bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        final Canvas canvas = new Canvas(output);
+
+        final int color = Color.RED;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        final RectF rectF = new RectF(rect);
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawOval(rectF, paint);
+
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+
+        bitmap.recycle();
+
+        return output;
     }
 }
